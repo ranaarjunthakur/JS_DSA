0521c5c74d81d801b757811fd69396b89d4d576f
arjun pratap rana <89635484+ranaarjunthakur@users.noreply.github.com>
2023-04-14 15:44:34 +0530

    Create BubbleSort.js

---
+++ BubbleSort.js
@@ -0,0 +1,18 @@
+////////////////////BUBBLE SORT/////////////////////////////////////
+
+var arr = [5,6,3,1,2,4];  
+  
+bubbleSortAlgo(arr);  
+  
+function bubbleSortAlgo(arr){  
+    for(var i=0;i<arr.length;i++){  
+        for(var j=0;j<arr.length-i-1;j++){  
+            if(arr[j]>arr[j+1]){  
+                var tempValue= arr[j];  
+                arr[j]=arr[j+1];  
+                arr[j+1]=tempValue;  
+            }  
+        }  
+    }  
+    console.log(arr);  
+}  

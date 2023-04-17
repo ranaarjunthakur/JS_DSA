e4bb05f0505afaed1b8e6589af354c91b6d131fc
arjun pratap rana <89635484+ranaarjunthakur@users.noreply.github.com>
2023-04-17 13:47:51 +0530

    Create SquaredElementArray.js

---
+++ SquaredElementArray.js
@@ -0,0 +1,24 @@
+//////////////////////////check if an array has squared elements of another array (regardless of order)?////////////////////////
+
+let arr1=[1,2,3,4]
+let arr2=[1,4,9,16]
+
+function isSquare(arr1,arr2){
+    for(let i=0; i<arr1.length;i++){
+        let isSquare = false;
+        for(let j=0; j<arr2.length;j++){
+            if(arr1[i]*arr1[i] === arr2[j]){
+                isSquare = true;
+            }
+            else if(j === arr2.length-1){
+                if(!isSquare){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+let res = isSquare(arr1,arr2)
+console.log(res)

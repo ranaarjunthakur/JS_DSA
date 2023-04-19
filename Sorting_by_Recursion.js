5bc62077de1a3e18fc4f8904695867e25dde9723
arjun pratap rana <89635484+ranaarjunthakur@users.noreply.github.com>
2023-04-19 17:13:43 +0530

    Create Sorting_by_Recursion.js

---
+++ Sorting_by_Recursion.js
@@ -0,0 +1,33 @@
+let myArray = [2, 8, 1, 3, 6, 7, 5, 4] 
+
+let myNewList=[]
+let i=0;
+let  j=1;
+
+function isSorted(array){
+    for(let i=0; i<array.length;i++){
+        if(array[i] > array[i+1]){
+            return false
+        }
+    }
+    return true;
+}
+
+function sortChecked(array){
+    if(isSorted(array)){
+        myNewList = array
+        return
+    }else if(array[i]<array[j]){
+        i++;
+        j++;
+        sortChecked(array)
+    }else{
+        [array[i],array[j]]=[array[j],array[i]]
+        i=0;
+        j=1;
+        sortChecked(array)
+    }
+}
+
+sortChecked(myArray)
+console.log(myNewList)

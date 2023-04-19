bf19f81be52b44eee649326453627ff0be031f25
arjun pratap rana <89635484+ranaarjunthakur@users.noreply.github.com>
2023-04-19 18:08:00 +0530

    changes

--- Sorting_by_Recursion.js
+++ Sorting_by_Recursion.js
@@ -2,11 +2,11 @@ let myArray = [2, 8, 1, 3, 6, 7, 5, 4]
 
 let myNewList=[]
 let i=0;
-let  j=1;
+let j=1;
 
 function isSorted(array){
     for(let i=0; i<array.length;i++){
-        if(array[i] > array[i+1]){
+        if(array[i] > array[i+1]){    // return false if array[i]>array[i+1]
             return false
         }
     }
@@ -16,13 +16,13 @@ function isSorted(array){
 function sortChecked(array){
     if(isSorted(array)){
         myNewList = array
-        return
-    }else if(array[i]<array[j]){
+        return;
+    }else if(array[i]<array[j]){               // check if array[i]<array[j]
         i++;
         j++;
         sortChecked(array)
     }else{
-        [array[i],array[j]]=[array[j],array[i]]
+        [array[i],array[j]]=[array[j],array[i]]            // swap values[ i,j [=[ j,i]
         i=0;
         j=1;
         sortChecked(array)

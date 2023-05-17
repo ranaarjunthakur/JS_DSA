f1bed1ff6145f724b01e63a96196bbc307a5f947
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2023-05-17 17:14:28 +0530

    Update Missing_Odd Numbers.js

--- Missing_Odd Numbers.js
+++ Missing_Odd Numbers.js
@@ -21,3 +21,18 @@ function findMissingOddNumbers(arr) {
 
 const missingOddNumbers = findMissingOddNumbers(input);
 console.log(missingOddNumbers); => [13]
+
+
+////////////////////////////////////////////////////////
+
+const input = [5,7,9,11,15,17,19];
+let outputs = 13
+
+let min = Math.min(...input)
+let max = Math.max(...input)
+
+for(let i=min;  i<=max; i++){
+    if(i % 2 !== 0 && !input.includes(i)){
+         console.log(i)
+    }
+}

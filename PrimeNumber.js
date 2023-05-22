c21e659f485cfbaa712f5a10f0a449a7da81a20f
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2023-05-22 12:13:42 +0530

    Create PrimeNumber.js

---
+++ PrimeNumber.js
@@ -0,0 +1,18 @@
+////////////////////////////////
+
+let n = 3;
+function isPrime(n){
+    
+    if(n<=1){
+        return false
+    }
+    for(let i=2; i< Math.sqrt(n);i++){
+        if(n%i==0){
+            return false
+        }
+    }
+     return true
+}
+
+let res2 = isPrime(n)
+console.log(res2)

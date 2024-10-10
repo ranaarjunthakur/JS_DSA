b0a9ca29f34300fed8711cb8f026c6fa76d5c0a3
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2024-10-10 14:34:24 +0530

    Update SubstringfromString.js

--- SubstringfromString.js
+++ SubstringfromString.js
@@ -26,6 +26,40 @@ function longestSubstring(str){
 console.log(longestSubstring(str))
 
 
+
+////////////////////////////second approach without set///////////////////////
+
+let str = "abcabcbb";
+
+function longestSubstring(str) {
+  let start = 0;
+  let maxLength = 0;
+  let longestChar = '';
+  let currentSub = '';  // Store current substring
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    // If the character is already in the current substring, update start position
+    if (currentSub.indexOf(char) !== -1) {
+      start = start + currentSub.indexOf(char) + 1;
+      currentSub = str.slice(start, i);  // Update current substring from new start
+    }
+
+    currentSub += char; // Add the current character
+
+    if (currentSub.length > maxLength) {
+      maxLength = currentSub.length;
+      longestChar = currentSub; // Save the longest substring
+    }
+  }
+
+  return { maxLength, longestChar };
+}
+
+console.log(longestSubstring(str));
+
+
 Output:
 
 { maxLength: 3, longestChar: 'abc' }

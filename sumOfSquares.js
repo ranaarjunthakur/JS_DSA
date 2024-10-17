9f0a1a001dee2fc239ea1aac63ce2027b36dc3ba
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2024-10-17 22:42:52 +0530

    Update sumOfSquares.js

--- sumOfSquares.js
+++ sumOfSquares.js
@@ -6,3 +6,20 @@ function sumOfSquares(vector) {
 let numericVector = [1, 2, 3, 4, 5];
 let result = sumOfSquares(numericVector);
 console.log(result); // Output: 55
+
+
+//////////////////////////////////////////////
+
+let arr=[1,2,3,4,5]
+
+function sumofSquare(arr){
+  
+  let sum=0;
+  
+  for(let val of arr){
+    sum +=val*val
+  }
+  return sum
+}
+
+console.log(sumofSquare(arr))

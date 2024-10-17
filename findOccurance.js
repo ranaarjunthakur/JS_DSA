a74bbfe60b9bc36b8ed00aba2afb6b22f2d31307
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2024-10-17 21:17:27 +0530

    Update findOccurance.js

--- findOccurance.js
+++ findOccurance.js
@@ -1,7 +1,7 @@
 let arr=[1,2,4,6,7,8,9,2,3,4,5,6,8,2]
 
 function occur(arr){
-    let result ={}
+    let result ={}https://github.com/ranaarjunthakur/JS_DSA/blob/main/findOccurance.js
     
     for(let val of arr){
         if(result[val]){
@@ -22,3 +22,19 @@ function occur(arr){
     return [val,max]
 }
 console.log(occur(arr))
+
+///////////////////////////////////////////////////////////////////////////
+
+ var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+function mostOccur(arr){
+  let obj={}
+  
+  for(let val of arr){
+   obj[val] ? ++obj[val] : (obj[val]=1);
+  }
+  
+  return Object.keys(obj).reduce((a,b)=> obj[a]>obj[b] ? a:b)
+}
+
+console.log(mostOccur(arr1))

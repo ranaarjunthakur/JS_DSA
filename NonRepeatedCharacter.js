9701cbb8f5137b98c0d580e4ce8d82bbdea56dd4
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2024-10-21 16:26:10 +0530

    Create NonRepeatedCharacter.js

---
+++ NonRepeatedCharacter.js
@@ -0,0 +1,38 @@
+let str = 'abacddbec';
+
+function nonRepeated(str) {
+  let charCount = {}; // Object to store character counts
+  let result = [];    // Array to store non-repeated characters
+
+  // Count the occurrences of each character
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  // Find the characters that appear only once
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (charCount[char] === 1) {
+      result.push(char);
+    }
+  }
+
+  return result;
+}
+
+console.log(nonRepeated(str)); // ['e']
+
+//////////////////////////////////////////////////////////////////////
+
+let str = 'abacddbec'
+
+function nonRepeated(str) {
+return str.split('').filter((val,index)=> str.indexOf(val) == str.lastIndexOf(val))
+}
+
+console.log(nonRepeated(str));

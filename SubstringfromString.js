beaf987898462dfdccbc5b87ffbf5e3fb619d163
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2025-04-08 15:50:21 +0530

    Update SubstringfromString.js

--- SubstringfromString.js
+++ SubstringfromString.js
@@ -1,3 +1,26 @@
+function longestSubstringWithoutRepeating(str) {
+  let longest = '';
+  let current = '';
+
+  for (let char of str) {
+    if (current.includes(char)) {
+      current = current.slice(current.indexOf(char) + 1);
+    }
+
+    current += char;
+
+    if (current.length > longest.length) {
+      longest = current;
+    }
+  }
+
+  return longest;
+}
+
+console.log(longestSubstringWithoutRepeating('abcabcbb')); // Output: 'abc'
+
+////////////////////////////////////////////////////////////////////////////////////////
+
 let str ="abcabcbb";
 
 function longestSubstring(str){

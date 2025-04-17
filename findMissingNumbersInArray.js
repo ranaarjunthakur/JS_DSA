c61f08f69b199e46ed7d0ce4c968cae773a8df05
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2025-04-17 10:17:52 +0530

    Update findMissingNumbersInArray.js

--- findMissingNumbersInArray.js
+++ findMissingNumbersInArray.js
@@ -23,3 +23,24 @@ console.log(missingNumber(arr))
 Output:
 
 [ 4, 7, 8, 10 ]
+
+////////////////////////////////////////////////////////
+
+
+let arr = [1, 2, 3, 5, 6, 8, 9];
+
+function missingNums(arr) {
+  let missing = [];
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+
+  for (let i = min; i <= max; i++) {
+    if (!arr.includes(i)) {
+      missing.push(i);
+    }
+  }
+
+  return missing;
+}
+
+console.log(missingNums(arr));

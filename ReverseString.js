913b48fd6f16341f0d3a0bcad17bc01a423c4e91
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2025-11-18 12:01:05 +0530

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -10,3 +10,17 @@ function reverseStr(str) {
 
 let res = reverseStr(str);
 console.log(res); // Output: 'ecruoser3w'
+
+
+//////////////////////Add Last 3 Characters to Front and Back of String/////////////////////////
+
+
+function addLast3FrontBack(str) {
+  if (str.length < 3) return str; // Not enough characters to extract last 3
+  let last3 = str.slice(-3);
+  return last3 + str + last3;
+}
+
+let original = 'w3resource';
+let modified = addLast3FrontBack(original);
+console.log(modified); // Output: 'rcew3resourcerce'

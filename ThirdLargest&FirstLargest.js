bdfeab29b86be696e6ee49075686f363ee67900e
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2025-12-02 15:50:31 +0530

    Update ThirdLargest&FirstLargest.js

--- ThirdLargest&FirstLargest.js
+++ ThirdLargest&FirstLargest.js
@@ -1,24 +1,3 @@
-let arr=[10,20,30,40,2,3,4,9]
-
-function thirdLargest(arr){
-    
-   let max1= Math.max(...arr)
-//   return max1
-   arr = arr.filter((val)=>val !==max1)
-//   return arr
-  let max2 = Math.max(...arr)
-  arr = arr.filter((val)=>val !==max2)
-//   return arr
-
-  return Math.max(...arr)
-   
-
-}
-
-let res = thirdLargest(arr)
-
-console.log(res)
-
 ///////////////////////FIND LARGEST VALUE///////////////////////////////////
 
 
@@ -34,3 +13,38 @@ function findLargest(arr) {
 }
 
 console.log(findLargest([99, 5, 3, 100, 1]));
+
+///////////////////////FIND SMALLEST AND LARGEST VALUE IN ARRAY/////////////////////////////////////////
+
+function findLargest(arr) {
+    let largest = arr[0]; 
+    let smallest = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i]; 
+        }
+        if(arr[i] < smallest){
+          smallest = arr[i]
+        }
+    }
+    return [smallest,largest]
+}
+
+console.log(findLargest([99, 5, 3, 100, 1]));
+
+//////////////////////////////////////////////////////////
+
+let arr=[10,20,30,40,2,3,4,9]
+
+function thirdLargest(arr){
+    
+   let max1= Math.max(...arr)
+   arr = arr.filter((val)=>val !==max1)
+  let max2 = Math.max(...arr)
+  arr = arr.filter((val)=>val !==max2)
+  return Math.max(...arr)
+}
+
+let res = thirdLargest(arr)
+console.log(res)
+

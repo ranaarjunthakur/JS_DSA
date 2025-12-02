a99fd2ec5ebdba328229b908f9cc1fc8e182cd5d
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2025-12-02 17:01:07 +0530

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -45,9 +45,9 @@ console.log(sorted);
 ////////////////////SORT ARRAY IN DESENDING ORDER////////////////////////////////////
 
 function sortArrayDesc(arr) {
-    let n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - 1 - i; j++) {
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
             if (arr[j] < arr[j + 1]) {
                [arr[j] , arr[j+1]] = [arr[j+1], arr[j]]
             }

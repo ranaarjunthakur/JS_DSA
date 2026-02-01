c7d2d6c7f65f56171d6a0f12d58ae49412a8ddc4
ARJUN PRATAP RANA <89635484+ranaarjunthakur@users.noreply.github.com>
2026-02-01 20:59:36 +0530

    Update flattenArray.js

--- flattenArray.js
+++ flattenArray.js
@@ -1,3 +1,44 @@
+///////////////////////////USING FLAT METHOD///////////////////////////////////////
+
+const flattened = array.flat();
+console.log(flattened);
+
+//////////////////////// USING INFINITY/////////////////////////////////////
+
+const arr = [1, [2, [3, [4]]]];
+console.log(arr.flat(Infinity));
+
+////////////////////////WITH FOR LOOP/////////////////////////////////////
+
+
+const flattened = [];
+
+for (let item of array) {
+  if (Array.isArray(item)) {
+    flattened.push(...item);
+  } else {
+    flattened.push(item);
+  }
+}
+
+console.log(flattened);
+
+
+//////////////////////////USING TERNARY OPERATOR /////////////////////////
+
+const array = [1, 2, [3, 4], [5, 6, 7]];
+
+let flat =[]
+
+for(let val of array){
+   Array.isArray(val) ? flat.push(...val) : flat.push(val)
+}
+
+console.log(flat)
+
+
+///////////////////////WITH RECURSION ///////////////////////////////////
+
 const array = [1, 2, [3, 4], [5, 6, 7]];
 
 let flat = (arr)=>{
@@ -23,3 +64,16 @@ Output:
   1, 2, 3, 4,
   5, 6, 7
 ]
+
+////////////////////////WITHOUT RECURSION/////////////////////////////////////
+
+const array = [1, 2, [3, 4], [5, 6, 7]];
+const flattened = [];
+for (let i = 0; i < array.length; i++) {
+  if (Array.isArray(array[i])) {
+    flattened.push(...array[i]);
+  } else {
+    flattened.push(array[i]);
+  }
+}
+console.log(flattened); 
